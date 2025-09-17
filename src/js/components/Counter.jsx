e73638b61b7b4c7241a7aee2d5c041b3230097a3
Avilarrealr <149917560+Avilarrealr@users.noreply.github.com>
2025-09-17 23:51:33 +0000

    Agregando funcionalidades necesarias del proyecto, mouse hover en el icono y logica en el count para mostrar mensaje en 0

--- src/js/components/Counter.jsx
+++ src/js/components/Counter.jsx
@@ -6,7 +6,7 @@ export const Counter = (props) => {
             <div className="row">
                 <div className="col">
                     <div className="border rounded border-bottom text-muted text-start p-3" style={{ boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)" }}>
-                        <span>item left {props.taskCount}</span>
+                        <span>{props.taskCount}</span>
                     </div>
                 </div>
             </div>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 			<div className="container" style={{ fontFamily: "roboto-light" }}>
 				<Input addTask={addTask} />
 				<List tasks={tasks} deleteTask={deleteTask} />
-				<Counter taskCount={tasks.length} />
+				<Counter taskCount={tasks.length == 0 ? "No hay tareas, añadir tareas" : ` items left: ${tasks.length}`} />
 				<div className="row">
 					<div className="col d-flex justify-content-center">
 						<div className="border border-bottom" style={{ width: "49vw", height: "0.8vh", boxShadow: "0px 3px 4px rgba(0, 0, 0, 0.2)" }}></div>
